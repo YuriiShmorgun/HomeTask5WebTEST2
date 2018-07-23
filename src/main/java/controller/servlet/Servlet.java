03cd9a3fda6421a53459e77022a412dff28f75cb
YuriiSH <yurashmorgun@gmail.com>
2018-07-23 09:33:30 +0300

    Initial commit

---
+++ src/main/java/controller/servlet/Servlet.java
@@ -0,0 +1,18 @@
+package controller.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*super.doGet(req, resp);*/
+        Writer writer = resp.getWriter();
+        writer.write("Hi im writer ");
+
+    }
+}
